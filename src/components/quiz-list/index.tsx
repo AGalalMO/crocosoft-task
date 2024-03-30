fbc82828269aa5d87b7cd7fdc97586dd62a970bf
AGalalMO <a.galal@propertiano.com>
2024-03-30 21:49:41 +0200

    rename folders and files

--- src/components/quiz-list/index.tsx
+++ src/components/quiz-list/index.tsx
@@ -1,25 +1,66 @@
-import Quizes from '../../data/quizes.json';
-import { useState } from 'react';
-import { QuizItem } from '../../types/quiz';
-import CreateQuiz from '../create-quiz';
-import Header from './components/header';
-import Quiz from './components/quiz';
+import Quizes from "../../data/quizes.json";
+import { useEffect, useState } from "react";
+import { EditableQuiz, QuizItem } from "../../types/quiz";
+import CreateQuiz from "../quiz-modal";
+import Header from "./components/ListHeader";
+import Quiz from "./components/QuizComponent";
+import { editableQuizInitValue } from "./utils";
+import { writeInJson } from "../../utils";
 export default function QuizList() {
-    let defaultQuiz=Quizes as QuizItem
-    const [quizItems, setQuizItems] = useState<QuizItem[]>([{...defaultQuiz}])
-    const [isQuizModalOpen, setIsQuizModalOpen] = useState(false)
-    const handleOpenModal = () => {
-        setIsQuizModalOpen(true)
-    }
-    const handleCloseModal = () => {
-        
-        setIsQuizModalOpen(false)
-    }
-    return (
-        <>
-         <Header handleOpenModal={handleOpenModal}  />
-           {quizItems?.map((quiz)=><Quiz quizItem={quiz}   />)}
-         <CreateQuiz isOpen={isQuizModalOpen}   handleCloseModal={handleCloseModal} />
-        </>
-    )
+	let defaultQuiz = Quizes as QuizItem;
+	const [quizItems, setQuizItems] = useState<QuizItem[]>([{ ...defaultQuiz }]);
+	const [isQuizModalOpen, setIsQuizModalOpen] = useState(false);
+	const [editableQuiz, setEditableQuiz] = useState<EditableQuiz>({
+		...editableQuizInitValue,
+	});
+	const handleOpenModal = () => {
+		setIsQuizModalOpen(true);
+	};
+	const handleCloseModal = () => {
+		setIsQuizModalOpen(false);
+	};
+
+	useEffect(() => {
+		return () => {
+			writeInJson(quizItems);
+		};
+	}, []);
+	return (
+		<>
+			<Header handleOpenModal={handleOpenModal} />
+			{quizItems?.map((quiz, index) => (
+				<Quiz
+					quizItem={quiz}
+					index={index}
+					editQuiz={() => {
+						setEditableQuiz({
+							isEdit: true,
+							index: index,
+							quiz: quiz,
+						});
+						setIsQuizModalOpen(true);
+					}}
+				/>
+			))}
+			{isQuizModalOpen ? (
+				<CreateQuiz
+					addQuiz={({ isEdit, index, quiz }) => {
+						if (isEdit) {
+							let quizes = [...quizItems];
+							quizes[index as number] = quiz;
+							setQuizItems([...quizes]);
+						} else setQuizItems([...quizItems, quiz]);
+					}}
+					defaultQuiz={editableQuiz}
+					isOpen={isQuizModalOpen}
+					handleCloseModal={() => {
+						handleCloseModal();
+						setEditableQuiz({ ...editableQuizInitValue });
+					}}
+				/>
+			) : (
+				""
+			)}
+		</>
+	);
 }
--- src/types/question.ts
+++ src/types/question.ts
@@ -1,12 +1,10 @@
 import { Answer } from "./answer";
 
-export interface Question{
-    answer_id: number|null;
-    answers: Answer[];
-    feedback_false: string;
-    feedback_true: string;
-    id: number;
-    text: string;
-
-
+export interface Question {
+	answer_id: number | null;
+	answers: Answer[];
+	feedback_false: string;
+	feedback_true: string;
+	id: number | null;
+	text: string;
 }
